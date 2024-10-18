bef88ad3c10e3a4feaa197a2d376db45ffee34d7
Oğuz Kurt <oguzkurt671@gmail.com>
2024-10-18 16:14:52 +0300

    labels and function indexes works now.

--- src/main/java/com/nlang/vm/Main.java
+++ src/main/java/com/nlang/vm/Main.java
@@ -1,43 +1,15 @@
 package com.nlang.vm;
 
-import static com.nlang.vm.InstructionSet.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int[] code = {
-                // if (n<2) return n;
-                LOAD, 0,        //0
-                IPUSH, 2,       //2
-                ILT,            //4
-                JZ, 10,         //5
-                LOAD, 0,       //7
-                RET,            //9
-                // return fib(n-1) + fib(n-2);
-                LOAD, 0,        //10
-                IPUSH,1,
-                ISUB,
-                CALL,1,
-                LOAD, 0,        //12
-                IPUSH, 2,       //14
-                ISUB,           //16
-                CALL, 1,        //17
-                IADD,           //19
-                RET,            //20
-
-                IPUSH, 23,       //21 MAIN
-                CALL, 1,        //23 CALL <Function_Index>
-                PRINT,          //25
-                STOP,           //26
-        };
-
-
-        FunctionInfo[] functionTable = {
-                new FunctionInfo("main", 0, code.length - 6),
-                new FunctionInfo("fibonacci", 1, 0)
-        };
-        NVM vm = new NVM(code, functionTable);
-        vm.execute();
+        String s = Files.readString(Paths.get("simple.nbyte"));
+        System.out.println(s);
 
     }
 }
